fb9c3b49f48cb22c3c0f9017ebcf0adf81a348d2
Özge Çelik <108154759+ozgecelk01@users.noreply.github.com>
2023-08-13 16:37:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ photo_name=input("Enter photo name with .jpeg: ")
 photo = cv2.imread(photo_name)
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
 faces = face_cascade.detectMultiScale(photo, scaleFactor=1.075, minNeighbors=5, minSize=(15,15))
 
 for(x,y,w,h) in faces:
   cv2.rectangle(photo, (x,y),(x+w,y+h), (255,0,0),2 )
   roi = photo[y:y+h, x:x+w]
+  gozler = eye_cascade.detectMultiScale(roi)
+  for(ex,ey,ew,eh) in gozler:
+    cv2.rectangle(roi, (ex,ey), (ex+ew, ey+eh), (0,255,0),1)
 
 print("Number Of Faces = ",format(len(faces)))
 
