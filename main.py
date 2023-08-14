d7b9ad463665b4dd7e3d654c3f51d72d1d369ebf
Özge Çelik <108154759+ozgecelk01@users.noreply.github.com>
2023-08-14 10:48:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-import cv2
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
-photo_name=input("Enter photo name with .jpeg: ")
-photo = cv2.imread(photo_name)
+data = pd.read_csv("ev_fiyat_tahmini.csv",sep=";")
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+x = data['MetreKare'].values.reshape((-1, 1))
+y = data['fiyatlar'].values.reshape((-1, 1))
 
-faces = face_cascade.detectMultiScale(photo, scaleFactor=1.075, minNeighbors=5, minSize=(15,15))
+model = LinearRegression()
+model.fit(x,y)
 
-for(x,y,w,h) in faces:
-  cv2.rectangle(photo, (x,y),(x+w,y+h), (255,0,0),2 )
-  roi = photo[y:y+h, x:x+w]
-  gozler = eye_cascade.detectMultiScale(roi)
-  for(ex,ey,ew,eh) in gozler:
-    cv2.rectangle(roi, (ex,ey), (ex+ew, ey+eh), (0,255,0),1)
-
-print("Number Of Faces = ",format(len(faces)))
-
-plt.figure(figsize=(8,12))
-plt.imshow(photo)
+sns.regplot(x="MetreKare", y="fiyatlar", data=data,line_kws={"color": "red"});
 plt.show()
